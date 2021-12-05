6827d3be5ea9256364b4072342f03d9f9cdf0a59
r0cc4t0 <guilherme.roccato@outlook.com>
2021-12-05 20:15:21 -0300

    Application Structure with React Native

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>Hello, World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  title: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontStyle: 'italic'
+  }
+});
---
+++ README.md
@@ -0,0 +1,4 @@
+# GamePlay
+
+GamePlay is an application to help you connect, organize your fun time, and play with friends.
+Create groups to play your favorite games with your friends with this application that has Discord authentication.
